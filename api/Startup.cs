b806b8f06aa018f220be026a74f445c956958105
Yousef Alaqra <56271934+yousefalaqra@users.noreply.github.com>
2020-04-08 11:52:16 +0300

    Setup api project, Setup angular web api

--- api/Startup.cs
+++ api/Startup.cs
@@ -34,12 +34,27 @@ namespace api
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped(typeof(IItemManager), typeof(ItemManager));
 
+
               // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Online Shopping API", Version = "v1" });
             });
-        }
+
+            services.AddMvc();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("swagger",
+                builder =>
+                {
+                    // Not a permanent solution, but just trying to isolate the problem
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+
+
+            
+            }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -63,12 +78,14 @@ namespace api
 
             app.UseRouting();
 
+            app.UseCors("swagger");
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute("api", "api/{controller}/{action}/{id?}");
-            });
+            }); 
 
         }
     }
